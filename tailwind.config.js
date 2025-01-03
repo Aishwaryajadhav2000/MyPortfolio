7f75ac867c9813316dc94e28742a0dfff731b75d
aishwarya <aishwaryaj1608@gmail.com>
2025-01-03 22:51:03 +0530

    new changes

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/myportfolio.html"],
+  theme: {
+    extend: {},
+  },
+  darkMode: 'class',
+  plugins: [],
+}
+
